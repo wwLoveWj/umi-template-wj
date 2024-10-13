e7855a1eb3b3093e92007346371dda54c43fcc52
wwLoveWj <blww885@163.com>
2024-10-13 17:28:27 +0800

    feat: 接入layout布局及公共函数

--- src/layouts/SecurityLayout.tsx
+++ src/layouts/SecurityLayout.tsx
@@ -1,21 +1,139 @@
-import { Link, Outlet } from "umi";
+// import { Link, Outlet } from "umi";
 import styles from "./index.less";
+// import { removeToken } from "@/utils/localToken";
+// import { UserOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { message, notification } from "antd";
+import React, { useEffect } from "react";
+import { history, useLocation } from "umi";
+import { storage } from "@/utils/storage";
+import { PROJECT_CONFIG } from "@/constants/constant";
+import { WjLayout } from "magical-antd-ui";
+import routes from "@/routes"; // 配置的菜单项
 
+// 获取到所有的菜单数据进行处理
+const menus =
+  routes
+    ?.find((route) => route.path === "/")
+    ?.routes?.filter((item: any) => !item.redirect) || [];
 export default function Layout() {
+  // const mailInfo = storage.get("login-info");
+  // const { pathname } = useLocation();
+  // 获取当前项目根节点
+  const homeWrapper = document.getElementById(
+    PROJECT_CONFIG?.NAME
+  ) as HTMLElement;
+  const ChangeUserOperation = () => {
+    const callEvent = () => {
+      localStorage.setItem("lastTime", new Date().getTime().toString());
+    };
+
+    homeWrapper.addEventListener("click", callEvent);
+    homeWrapper.addEventListener("keydown", callEvent);
+    homeWrapper.addEventListener("mouseover", callEvent);
+    homeWrapper.addEventListener("mousewheel", callEvent);
+  };
+  useEffect(() => {
+    //windows上设置一个循环定时器，每隔一秒调用一次监听函数，并定义在全局global上,用于超时后清除
+    ChangeUserOperation();
+    const CheckOpartionTimer = setInterval(() => {
+      const _lastTime =
+        (Number(localStorage.getItem("lastTime")) as number) * 1;
+      const nowTime = new Date().getTime();
+      console.log(
+        _lastTime,
+        "nowTime----------------------------------",
+        nowTime
+      );
+      if (nowTime - _lastTime > 1000 * 1000) {
+        console.log(
+          "当前时间：",
+          nowTime,
+          "最新时间",
+          _lastTime,
+          "间隔：",
+          nowTime - _lastTime,
+          "超时了，已退出登录"
+        );
+        message.warning("长时间未操作，即将退出登录");
+        // notification.error({ message: "'长时间未操作，已退出登录'" });
+        // 清除计时器
+        clearInterval(CheckOpartionTimer);
+
+        // 延迟一段时间后再进行导航
+        setTimeout(() => {
+          // 断开连接，退出
+          history.push("/login");
+        }, 2000); // 延迟2秒
+      }
+    }, 1000);
+    return () => {
+      clearInterval(CheckOpartionTimer);
+      localStorage.clear();
+      homeWrapper.removeEventListener("click", function () {});
+      homeWrapper.removeEventListener("keydown", function () {});
+      homeWrapper.removeEventListener("mouseover", function () {});
+      homeWrapper.removeEventListener("mousewheel", function () {});
+    };
+  }, []);
+
+  // 退出登录操作
+  // const loginOut = () => {
+  //   const { location } = history;
+  //   const search = location?.search
+  //     ? location?.search.substring(0, 1) === "?"
+  //       ? location.search
+  //       : `?${location.search}`
+  //     : "";
+  //   window.location.href = `${window.location.origin}${
+  //     mailInfo?.loginPath || "/login"
+  //   }?redirect=${pathname}${search}`;
+  //   storage.del("login-info");
+  //   storage.del("menuList");
+  //   removeToken();
+  // };
+
+  // settings的菜单
+  const avatarItems: MenuProps["items"] = [
+    {
+      key: "1",
+      label: (
+        <a
+          onClick={() => {
+            history.push("/login");
+            storage.del("login-info");
+            storage.del("menuList");
+            localStorage.clear();
+          }}
+        >
+          退出登录
+        </a>
+      ),
+    },
+    {
+      key: "2",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={() => {
+            history.push("/center");
+          }}
+        >
+          个人中心
+        </a>
+      ),
+    },
+  ];
   return (
     <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      <Outlet />
+      <WjLayout
+        isShowHeader={false}
+        avatarItems={avatarItems}
+        // rolesList={rolesList}
+        routes={menus}
+        projectName={PROJECT_CONFIG.TITLE}
+      />
     </div>
   );
 }
---
+++ src/utils/index.tsx
@@ -0,0 +1,47 @@
+import JSEncrypt from "jsencrypt";
+
+export const toLoginPage = () => {};
+
+export const redirectUrl = (userType: 1 | 2 | 3 | 4) => {
+  const redirectMap = new Map([
+    [1, "/admin/svip"],
+    [2, "/admin/vip"],
+    [3, "/admin/permission"],
+    [4, "/users"],
+  ]);
+  return redirectMap.get(userType) || "/users";
+};
+
+// 解密
+export const getPrivateKey = (data: string) => {
+  const decrypt = new JSEncrypt(); //创建解密对象实例
+  //之前生成的秘钥(把自己生成的密钥钥粘到对应位置)
+  const priKey = `MIICXQIBAAKBgQC0w6ohy8KMB+Zm8msT7JK/r7boeq1yVfhT0NVrJ/SmqZSeGYcK
+  BAY0uC4MEnZDmbcThrXC1OL+OY5H/siEQQ/RUFeBr9PYKdFvtaeonw2dqp4mIaPx
+  r/WRD19hdJQ8pRhyu4E3nINz21VSMG+Fsq5IEnFXHSmZnXo0c8EnjkPnDwIDAQAB
+  AoGAMn3dDeeehgLG5sacBkq0ivqH+tQZkrBU1wSkOHRGsC76Jlc3EyMgoKU5p4GY
+  qzVKoBemua9BYIWvKokOdXK++d5H1TcqoM5vlg96tVGbWjQsBOB5vzrTL3iB9phm
+  UQwPLlhQdlD8Oe0APCVEmcjGu4bZeOsjQcUKRQh6r68hEzECQQDhzb5gU8pfST9d
+  yklhswKrYYTxCT3Bk0cWHXzeNJRhKx+LTnxER1j2/y2U1GDn2sD0HF8/fOp1wxio
+  1b5AD3E9AkEAzPAGSEIorrKG5G0QwRs6/Hb8eUJMC+Kpk24nAjWSXEHB89RwiQ+f
+  I9oOfcfZ9L6aT6CSVHv6P2eJKzh91ljmOwJBAMQVVX/y42cxhVxeALFdsTDAQ6DI
+  xJ8n1PBIdRnFESD8QK1JX7IvPvtmht+aOfi9Rxf012WbhUL0Q6LB0i9w+YUCQQCI
+  hVUKCdikGBW4UdeZbJRs3q7t8HZMbKBesECYPivT/ZJA5kJZa+itr3N74wiU++w8
+  I1GXqkdYuZYtDqL1iZ9FAkA+P6RELAyLpsLYugHoDp1/pwsj3O3wIE67X21FWYn6
+  +PIY817HUgWNc21HXWliWkkgMZTMHPXcyJbLy8+ClXMj`;
+  decrypt.setPrivateKey(priKey); //设置秘钥
+  const uncrypted = decrypt.decrypt(data); //解密之前拿公钥加密的内容
+  return uncrypted;
+};
+// 加密
+export const setPrivateKey = (content: any) => {
+  const encryptor = new JSEncrypt(); // 创建加密对象实例
+  //之前生成的公钥，复制的时候要小心不要有空格(此处把密钥省略了，自己写的时候可把自己生成的公钥粘到对应位置)
+  const pubKey = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC0w6ohy8KMB+Zm8msT7JK/r7bo
+  eq1yVfhT0NVrJ/SmqZSeGYcKBAY0uC4MEnZDmbcThrXC1OL+OY5H/siEQQ/RUFeB
+  r9PYKdFvtaeonw2dqp4mIaPxr/WRD19hdJQ8pRhyu4E3nINz21VSMG+Fsq5IEnFX
+  HSmZnXo0c8EnjkPnDwIDAQAB`;
+  encryptor.setPublicKey(pubKey); //设置公钥
+  const rsaPassWord = encryptor.encrypt(content); // 对内容进行加密
+  return rsaPassWord;
+};
