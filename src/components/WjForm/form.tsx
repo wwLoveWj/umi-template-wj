4257b026542a03be4ff728a30b5fd40e8af9880a
wwLoveWj <blww885@163.com>
2024-10-14 19:20:06 +0800

    feat: form组件的完结

--- src/components/WjForm/form.tsx
+++ src/components/WjForm/form.tsx
@@ -27,17 +27,97 @@ interface FormItemConfigLsitType {
   formItemProps?: any;
   fieldProps?: any;
 }
+/**
+ * @description 表单传参示例
+ * const formConfigList: FormItemConfigLsitType[] = [
+    {
+      type: "search",
+      name: "username",
+      label: "用户名",
+      formItemProps: {
+        rules: [{ required: true, message: "用户名必填" }],
+      },
+      fieldProps: {
+        placeholder: "请输入用户名",
+        onSearch: (value: string) => {
+          console.log("search");
+        },
+      },
+    },
+    {
+      type: "input",
+      name: "username",
+      label: "用户名",
+      formItemProps: {
+        rules: [{ required: true, message: "请输入用户名" }],
+      },
+    },
+    {
+      type: "number",
+      name: "age",
+      label: "年龄",
+      formItemProps: {
+        rules: [{ required: true, message: "请输入年龄" }],
+      },
+    },
+    {
+      type: "password",
+      name: "username",
+      label: "用户密码",
+      formItemProps: {
+        rules: [{ required: true, message: "请输入用户密码" }],
+      },
+    },
+    {
+      type: "select",
+      name: "userType",
+      label: "用户类型",
+      formItemProps: {
+        rules: [{ required: true, message: "请选择用户类型" }],
+      },
+      fieldProps: {
+        options: [
+          {
+            label: "超级管理员",
+            value: "1",
+          },
+          {
+            label: "普通用户",
+            value: "2",
+          },
+        ],
+      },
+    },
+    {
+      type: "textarea",
+      name: "description",
+      label: "描述",
+      formItemProps: {
+        rules: [{ required: true, message: "请输入描述" }],
+      },
+      fieldProps: {
+        placeholder: "请输入描述",
+      },
+    },
+  ];
+ */
 export type { FormItemConfigLsitType };
 export default function Index({
   formConfigList,
   form: formInstance,
   successNotifyProps,
   successNotify = true,
+  btnConfig,
 }: {
   formConfigList: FormItemConfigLsitType[];
-  form: FormInstance;
-  successNotify: boolean;
-  successNotifyProps: any;
+  form?: FormInstance;
+  successNotify?: boolean;
+  successNotifyProps?: any;
+  btnConfig: {
+    onCancel?: () => void;
+    cancelTxt?: string;
+    submitTxt?: string;
+  };
 }) {
   const [form] = Form.useForm(formInstance);
   /**
@@ -59,7 +139,7 @@ export default function Index({
     console.error("Failed:", errorInfo);
     form?.scrollToField(
       { name: errorInfo?.errorFields?.[0]?.name },
-      { block: "center" }
+      { block: "center", behavior: "smooth" }
     );
   };
   /**
@@ -103,7 +183,35 @@ export default function Index({
     onFinishFailed,
     autoComplete: "off",
     ...formItemLayout,
-    children: fromItemRender(config),
+    children: {
+      component: "row",
+      gutter: [0, 20],
+      justify: "end",
+      children: [
+        ...fromItemRender(config),
+        {
+          component: "space",
+          children: [
+            {
+              component: "button",
+              span: 12,
+              children: btnConfig?.submitTxt || "查询",
+              type: "primary",
+              htmlType: "submit",
+            },
+            {
+              component: "button",
+              span: 12,
+              children: btnConfig?.cancelTxt || "重置",
+              onClick: () => {
+                (btnConfig?.onCancel && btnConfig?.onCancel()) ||
+                  form.resetFields();
+              },
+            },
+          ],
+        },
+      ],
+    },
   });
   const schema = () => {
     //   是否需要卡片包裹
--- src/pages/collect/index.tsx
+++ src/pages/collect/index.tsx
@@ -1,5 +1,93 @@
 import React from "react";
-
+import { WjForm, FormItemConfigLsitType } from "@/components/WjForm";
+import { history } from "umi";
 export default function Index() {
-  return <div>收藏管理</div>;
+  const formConfigList1: FormItemConfigLsitType[] = [
+    {
+      type: "search",
+      name: "username",
+      label: "用户名",
+      formItemProps: {
+        rules: [{ required: true, message: "用户名必填" }],
+      },
+      fieldProps: {
+        placeholder: "请输入用户名",
+        onSearch: (value: string) => {
+          console.log("search");
+        },
+      },
+    },
+    {
+      type: "input",
+      name: "username",
+      label: "用户名",
+      formItemProps: {
+        rules: [{ required: true, message: "请输入用户名" }],
+      },
+    },
+    {
+      type: "number",
+      name: "age",
+      label: "年龄",
+      formItemProps: {
+        rules: [{ required: true, message: "请输入年龄" }],
+      },
+    },
+    {
+      type: "password",
+      name: "username",
+      label: "用户密码",
+      formItemProps: {
+        rules: [{ required: true, message: "请输入用户密码" }],
+      },
+    },
+    {
+      type: "select",
+      name: "userType",
+      label: "用户类型",
+      formItemProps: {
+        rules: [{ required: true, message: "请选择用户类型" }],
+      },
+      fieldProps: {
+        options: [
+          {
+            label: "超级管理员",
+            value: "1",
+          },
+          {
+            label: "普通用户",
+            value: "2",
+          },
+        ],
+      },
+    },
+    {
+      type: "textarea",
+      name: "description",
+      label: "描述",
+      formItemProps: {
+        rules: [{ required: true, message: "请输入描述" }],
+      },
+      fieldProps: {
+        placeholder: "请输入描述",
+      },
+    },
+  ];
+
+  return (
+    <div>
+      收藏管理
+      <WjForm
+        formConfigList={formConfigList1}
+        btnConfig={{
+          submitTxt: "提交",
+          cancelTxt: "取消",
+          onCancel: () => {
+            debugger;
+            history.push("/article");
+          },
+        }}
+      />
+    </div>
+  );
 }
