ea7b1f8eb60fee518fe6074d72c9e30ee23ac7d4
wwLoveWj <blww885@163.com>
2024-12-01 11:53:55 +0800

    feat: 元素周期表发光

---
+++ src/components/WjImages/calendar/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.less";
+export default function Index() {
+  return (
+    <div className="gas he">
+      <span className="number">11月</span>
+      <div className="symbol">周六</div>
+      <p className="name">30日</p>
+    </div>
+  );
+}
