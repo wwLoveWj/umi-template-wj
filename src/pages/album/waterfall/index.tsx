4c6dadef860246c5429d223454bd66fefa1a0b8d
wwLoveWj <blww885@163.com>
2025-03-29 16:13:46 +0800

    feat: 宝宝相册

--- src/pages/album/waterfall/index.tsx
+++ src/pages/album/waterfall/index.tsx
@@ -1,5 +1,27 @@
 import React from "react";
-
+import { imgInfoQueryAPI } from "@/service/api/file";
+import { useRequest } from "ahooks";
+import "./style.less";
 export default function Waterfall() {
-  return <div>瀑布流</div>;
+  // 查询图片上传列表
+  const { data: imageUrlList } = useRequest(imgInfoQueryAPI);
+  return (
+    <div className="waterfall">
+      <div className="tall">
+        <img src={require("@/assets/imgs/login.png")} alt="" />
+        <span>1</span>
+      </div>
+      {imageUrlList?.map((item) => (
+        <div>
+          <img
+            src={item?.imgUrl}
+            alt="文件上传图片"
+            id={item?.imgId}
+            loading="lazy"
+          />
+          <span>{item?.id}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
